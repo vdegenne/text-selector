7c427b11eba8b337ae53ff4948c8b4c1caedd991
vdegenne <vdegenne@gmail.com>
2026-03-28 08:11:33 +0000

    Build: (2afca1f) highlight colors

--- assets/page-404-DsRYQavE.js
+++
@@ -1,1 +0,0 @@
-import{h as e,m as t,t as n,u as r}from"./store-DNCnMKke.js";import{h as i,u as a,y as o}from"./directive-lPw2sjmk.js";import{t as s}from"./PageElement-rv_h3cgq.js";var c=class extends s{render(){return i`404 NOT FOUND`}};c=r([a(`page-404`),e(n),t(o``)],c);export{c as Page404};
