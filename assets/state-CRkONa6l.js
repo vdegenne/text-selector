da1a765631b07bdc9bc5c4b9f5f46f002c4e40c2
vdegenne <vdegenne@gmail.com>
2026-03-31 20:14:59 +0000

    Build: (6c6394e) box-border

---
+++ assets/state-CRkONa6l.js
@@ -0,0 +1,1 @@
+import{n as r}from"./index-DL_T9Xx0.js";function n(t){return r({...t,state:!0,attribute:!1})}export{n as r};
--- assets/state-ykZevV8U.js
+++
@@ -1,1 +0,0 @@
-import{n as r}from"./index-BIzIUgdB.js";function n(t){return r({...t,state:!0,attribute:!1})}export{n as r};
