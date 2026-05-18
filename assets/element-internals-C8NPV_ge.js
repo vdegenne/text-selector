f2539b61e52ee5aa00729f739dc4cc7d956fc143
vdegenne <vdegenne@gmail.com>
2026-05-18 15:27:10 +0000

    Build: (e56e27f) jisho

--- assets/element-internals-C8NPV_ge.js
+++
@@ -1,1 +0,0 @@
-const s=Symbol("internals"),t=Symbol("privateInternals");function i(n){class e extends n{get[s](){return this[t]||(this[t]=this.attachInternals()),this[t]}}return e}export{s as i,i as m};
