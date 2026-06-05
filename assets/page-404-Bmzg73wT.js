fc839ada78197170340edc4452028be1018cbde8
vdegenne <vdegenne@gmail.com>
2026-06-05 16:27:10 +0000

    Build: (2df77c0) ...

--- assets/page-404-Bmzg73wT.js
+++
@@ -1,1 +0,0 @@
-import{A as e,E as t,T as n,j as r,n as i,t as a,w as o,x as s}from"./index-B7JHy7zt.js";var c=class extends a{render(){return e`404 NOT FOUND`}};c=o([s(`page-404`),t(i),n(r``)],c);export{c as Page404};
