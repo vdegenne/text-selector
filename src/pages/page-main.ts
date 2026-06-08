fc625071e83da2c5c2a5a56b27254035e6ac8888
Valentin Degenne <vdegenne@gmail.com>
2026-06-08 12:28:12 +0200

    try a better coloring?

--- src/pages/page-main.ts
+++ src/pages/page-main.ts
@@ -46,12 +46,14 @@ declare global {
 		background-color: var(--md-sys-color-secondary-container);
 		background-color: var(--md-sys-color-outline);
 		background-color: var(--md-sys-color-secondary);
+		background-color: var(--md-sys-color-outline);
 		color: #ff0000;
 		color: #ab9a00;
 		color: var(--md-sys-color-on-primary-container);
 		color: var(--md-sys-color-on-secondary-container);
 		color: var(--md-sys-color-inverse-on-surface);
 		color: var(--md-sys-color-on-secondary);
+		color: var(--md-sys-color-on-surface);
 	}
 `)
 export class PageMain extends PageElement {
