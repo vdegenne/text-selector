29114209bf793000529ea7740c572d18393b2a63
Valentin Degenne <vdegenne@gmail.com>
2026-07-12 14:33:43 +0200

    hum

--- src/global-listeners.ts
+++ src/global-listeners.ts
@@ -1,6 +1,9 @@
 import {cquerySelector} from 'html-vision'
 import {askGemini} from './gemini.js'
 import {openSettingsDialog} from './imports.js'
+import {mainPage} from './pages/page-main.js'
+import toast from 'toastit'
+import {hasSomeJapanese} from 'asian-regexps'
 
 const inputNames = ['INPUT', 'TEXTAREA', 'MD-FILLED-TEXT-FIELD']
 export function eventIsFromInput(event: Event) {
@@ -37,12 +40,12 @@ window.addEventListener('keypress', async (event: KeyboardEvent) => {
 			break
 
 		case 't':
-			const response = await askGemini(
-				'590さんでエイムビジョン買っても流石におもんない？？？',
-				{
-					apiKey: '<INSERT_YOUR_GEMINI_API_KEY_HERE>',
-				},
-			)
+			const sentence = mainPage.getContent()
+			if (!hasSomeJapanese(sentence)) return
+
+			const response = await askGemini(sentence, {
+				apiKey: '<insert_api_key>',
+			})
 			console.log(response)
 			break
 	}
