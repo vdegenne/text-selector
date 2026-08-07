52e019c79a61d3ba7db6b16872e65f979056c2dd
Valentin Degenne <vdegenne@gmail.com>
2026-08-07 20:54:12 +0200

    get page from shadow

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -5,7 +5,7 @@ export const availablePages = ['main'] as const
 export type Page = (typeof availablePages)[number]
 
 export function getPage(name: Page) {
-	return $(`page-${name}`)
+	return $(`page-${name}`, {shadows: true})
 }
 export function getMainPage() {
 	return getPage('main') as PageMain
